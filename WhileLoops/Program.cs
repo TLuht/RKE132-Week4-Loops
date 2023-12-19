65023180afa2082b4cbf7b65e131d0bd188864c6
TLuht <thomas.luht@tktk.ee>
2023-12-19 23:30:46 +0200

    Loops

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,22 @@
+﻿Random rnd = new Random();
+int cpuRandom;
+
+while(true)
+{
+    cpuRandom = rnd.Next(1, 4);
+    Console.WriteLine("Make a guess. Enter a number 1-3");
+
+    int userNum = Int32.Parse(Console.ReadLine());
+
+    if (userNum == cpuRandom)
+    {
+        Console.WriteLine("Congratulations, you won!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Try again");
+    }
+}
+
+Console.WriteLine("Bye bye!");
